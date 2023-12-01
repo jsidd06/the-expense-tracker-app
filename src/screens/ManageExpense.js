b85ffcaccfad84fbba2329bbbbeb85f3a470578b
Siddharth Jain <jsidd06@gmail.com>
2023-12-01 05:37:40 +0530

    feat: add a reusable button

--- src/screens/ManageExpense.js
+++ src/screens/ManageExpense.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 import React, { useLayoutEffect } from "react";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/style";
+import Button from "../components/UI/Button";
 
 const ManageExpenseScreen = ({ route, navigation }) => {
   const expensesId = route?.params?.expenseId;
@@ -13,14 +14,35 @@ const ManageExpenseScreen = ({ route, navigation }) => {
     });
   }, [navigation, isEditing]);
 
+  const deleteHandler = () => {
+    navigation.goBack();
+  };
+
+  const cancelHandler = () => {
+    navigation.goBack();
+  };
+
+  const confirmHandler = () => {
+    navigation.goBack();
+  };
+
   return (
     <View style={styles.rootCtn}>
+      <View style={styles.btnCtn}>
+        <Button mode="flat" style={styles.button} onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
+      </View>
       {isEditing && (
         <View style={styles.subCtn}>
           <IconButton
             iconName="trash"
             size={24}
             color={GlobalStyles.colors.error50}
+            onPress={deleteHandler}
           />
         </View>
       )}
@@ -42,5 +64,14 @@ const styles = StyleSheet.create({
     borderTopWidth: 2,
     borderTopColor: GlobalStyles.colors.primary200,
     alignItems: "center",
+  },
+  btnCtn: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
 });
